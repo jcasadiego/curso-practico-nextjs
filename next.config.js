8d4727e4814d2b9506f67b24adde6b5ed54a4310
jcasadiego <jcasadiego.a@gmail.com>
2022-11-22 06:59:28 -0400

    agregando config iniciales

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://github.com/jcasadiego',
+        permanent: true,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
